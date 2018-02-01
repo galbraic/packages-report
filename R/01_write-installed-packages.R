e65461cd85fe7ff8e5c2dd01799b571db974d6fb
galbraic <galbraic@uci.edu>
2018-02-01 11:41:48 -0800

    write installed packages to file

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,9 @@
+library(here)
+
 ## deja vu from yesterday!
 
 ## create a data frame of your installed packages
+pkg <- as.data.frame(installed.packages())
 
 ## keep the variables
 ##   * Package
@@ -8,11 +11,14 @@
 ##   * Version
 ##   * Priority
 ##   * Built
+pkg <- pkg[, c("Package", "LibPath", "Version", "Priority", "Built")]
 
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+write.csv(pkg, file=here("data", "installed-packages.csv"),
+          quote = FALSE, row.names = FALSE)
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
